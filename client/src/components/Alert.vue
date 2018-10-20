ff9765037ac29ce19b2fbae9115bdd2af938b598
Wesley Hitson <wesleyhitson@users.noreply.github.com>
2018-10-20 19:12:02 -0400

    Finished basic tutorial

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-alert show dismissible variant="success">{{ message }}</b-alert>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
